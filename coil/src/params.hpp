bd3aba73d30a026f7635b99d246525f3766581a9
dm <murzzz@gmail.com>
2013-02-16 18:19:35 +0200

    cleanup

--- coil/src/params.hpp
+++ coil/src/params.hpp
@@ -21,34 +21,9 @@ struct board
     std::string board_str_;
 };
 
-//std::istream& operator>>(std::istream& in, board::square_type& square)
-//{
-//    std::string token;
-//    in >> token;
-//    std::cout << token << std::endl;
-//    for (const std::string::value_type& square_token : token)
-//    {
-//        board::square_type st = static_cast<board::square_type>(square_token);
-//
-//        switch (st)
-//        {
-//        case board::empty:
-//        case board::wall:
-//            square = st;
-//            break;
-//        default:
-//            throw boost::program_options::validation_error(
-//                    boost::program_options::validation_error::invalid_option_value);
-//        }
-//    }
-//
-//    return in;
-//}
-
 class exit_with_success: public std::exception
 {
-//public:
-    //help_printed
+
 };
 
 void parse(board& board, int argc, char** argv)
@@ -99,6 +74,7 @@ void parse(board& board, int argc, char** argv)
 
     po::notify(vm);
 
+    //convert board from str to enum
     for (const std::string::value_type& square_str : board.board_str_)
     {
         board::square_type square = static_cast<board::square_type>(square_str);
@@ -114,44 +90,5 @@ void parse(board& board, int argc, char** argv)
                     "board", board.board_str_);
         }
     }
-
-//        if (vm.count("help"))
-//        {
-//            std::cout << desc << "\n";
-//            //return 1;
-//        }
-//
-//        if (vm.count("compression"))
-//        {
-//            std::cout << "Compression level was set to "
-//                    << vm["compression"].as<int>() << ".\n";
-//        }
-//        else
-//        {
-//            std::cout << "Compression level was not set.\n";
-//        }
-//
-//        // Declare a group of options that will be
-//        // allowed only on command line
-//        po::options_description generic("Generic options");
-//        generic.add_options()("version,v", "print version string")("help",
-//                "produce help message");
-//
-//        // Declare a group of options that will be
-//        // allowed both on command line and in
-//        // config file
-//        int opt;
-//        po::options_description config("Configuration");
-//        config.add_options()("optimization",
-//                po::value<int>(&opt)->default_value(10), "optimization level")(
-//                "include-path,I",
-//                po::value<std::vector<std::string> >()->composing(),
-//                "include path");
-//
-//        // Hidden options, will be allowed both on command line and
-//        // in config file, but will not be shown to the user.
-//        po::options_description hidden("Hidden options");
-//        hidden.add_options()("input-file",
-//                po::value<std::vector<std::string> >(), "input file");
 }
 
