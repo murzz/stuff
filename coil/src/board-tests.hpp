ab89841782236f099de33fa4367b847d41b6df39
murzzz <murzzz@gmail.com>
2013-03-13 11:19:00 +0200

    board and skeleton of its test are ready

--- coil/src/board-tests.hpp
+++ coil/src/board-tests.hpp
@@ -5,27 +5,23 @@
 
 #include "board.hpp"
 
-//boost::test_tools::predicate_result
-//compare_boards(board const& l1, board const& l2)
-//{
-//    if (l1.board_str_.size() != l2.board_str_.size())
-//    {
-//        boost::test_tools::predicate_result res(false);
-//
-//        res.message() << "Different sizes [" << l1.board_str_.size() << "!="
-//                << l2.board_str_.size()
-//                << "]";
-//
-//        return res;
-//    }
-//
-//    return true;
-//}
+boost::test_tools::predicate_result
+compare_boards(const board& l1, const board& l2)
+{
+   if (l1 == l2)
+   {
+      return true;
+   }
 
-//BOOST_AUTO_TEST_CASE( f )
-//{
-//    board b1;
-//    board b2;
-//    BOOST_CHECK(compare_boards(b1, b2));
-//}
+   boost::test_tools::predicate_result res(false);
+   res.message() << "boards not equal";
+   return res;
+}
+
+BOOST_AUTO_TEST_CASE( compare_boards_test )
+{
+   board b1;
+   board b2;
+   BOOST_CHECK(compare_boards(b1, b2));
+}
 
--- coil/src/board.hpp
+++ coil/src/board.hpp
@@ -39,11 +39,11 @@ public:
          //std::for_each(squares.begin(), squares.end(), [](const std::string::value_type& square_str)
          for (const auto& square_str : squares)
          {
-            square square = static_cast<squares::square>(square_str);
+            squares::square square = static_cast<squares::square>(square_str);
             switch (square)
             {
-               case square::empty:
-                  case square::wall:
+               case squares::square::empty:
+                  case squares::square::wall:
                   value_.push_back(square);
                break;
                default:
@@ -55,6 +55,11 @@ public:
          return *this;
       }
 
+      bool operator==(const squares& rhs) const
+      {
+         return value_ == rhs.value_;
+      }
+
       type& value()
       {
          return value_;
@@ -63,6 +68,12 @@ public:
    private:
       type value_;
    };
+
+   board() :
+         x_(0), y_(0)
+   {
+
+   }
 
    board(const size_t& x, const size_t& y, const std::string& squares)
    {
@@ -75,6 +86,16 @@ public:
       x_ = x;
       y_ = y;
       squares_ = squares;
+   }
+
+   bool operator==(const board& rhs) const
+   {
+      return x_ == rhs.x_ && y_ == rhs.y_ && squares_ == rhs.squares_;
+   }
+
+   bool operator!=(const board& rhs) const
+   {
+      return !(*this == rhs);
    }
 
 private:
