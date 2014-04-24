96d30fb596b3c4d049bece3b9a2b816f5ad8ecaf
dm <murzzz@gmail.com>
2014-04-24 22:39:11 +0300

    new downloader unit tests

--- coil/src/downloader-tests.hpp
+++ coil/src/downloader-tests.hpp
@@ -1,27 +1,15 @@
 #pragma once
 
-//#define BOOST_TEST_MODULE board tests
 #include <boost/test/unit_test.hpp>
 #include <boost/bind.hpp>
-#include <boost/function.hpp>
-#include <boost/asio/placeholders.hpp>
 
 #include "downloader.hpp"
 
-void html_handler(const boost::system::error_code & ec, const std::string & html)
+void ok_handler(const boost::system::error_code & ec, const std::string & html)
 {
-   if (ec)
-   {
-      //if (downloader::get_error_category() == ec.category())
-      {
-         BOOST_TEST_MESSAGE(ec.message());
-         std::cout << "error: " << ec.message() << std::endl;
-      }
-   }
-   std::cout << "html: " << html << std::endl;
    // no errors
    boost::system::error_code no_error;
-   BOOST_REQUIRE_EQUAL(ec, no_error);
+   BOOST_REQUIRE_MESSAGE(ec == no_error, ec.message());
 
    // html is here
    const bool is_html = std::string::npos != html.find("<html>")
@@ -29,13 +17,29 @@ void html_handler(const boost::system::error_code & ec, const std::string & html
    BOOST_REQUIRE_EQUAL(is_html, true);
 }
 
-BOOST_AUTO_TEST_CASE( downloader_test1 )
+BOOST_AUTO_TEST_CASE( download_html )
 {
    boost::asio::io_service io_service;
    //boost::asio::io_service::work work(io_service);
    downloader downloader(io_service);
-   const std::string url = "http://googssle.com";
-   downloader.get_html(url, boost::bind(html_handler, _1, _2));
+   const std::string url = "http://google.com";
+   downloader.get_html(url, boost::bind(ok_handler, _1, _2));
+
+   io_service.run();
+}
+
+void wrong_hostname_handler(const boost::system::error_code & ec, const std::string & html)
+{
+   boost::system::error_code no_host = downloader::make_error_code(CURLE_COULDNT_RESOLVE_HOST);
+   BOOST_REQUIRE_EQUAL(ec, no_host);
+}
+
+BOOST_AUTO_TEST_CASE( download_wrong_host )
+{
+   boost::asio::io_service io_service;
+   downloader downloader(io_service);
+   const std::string url = "http://unresolvable-hostname-for-sure-nyash-myash.com";
+   downloader.get_html(url, boost::bind(wrong_hostname_handler, _1, _2));
 
    io_service.run();
 }
