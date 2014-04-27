226eb1f612cdd5e1bae02e6b27b6d04d2f29ba65
dm <murzzz@gmail.com>
2014-04-27 18:14:30 +0300

    - program options finalized: 	- read from cmd line 	- from file 	- from env vars
    - error_handler incapsulated into worker

--- coil/src/cmdline-parser.hpp
+++ coil/src/cmdline-parser.hpp
@@ -5,10 +5,47 @@
 #include <boost/system/error_code.hpp>
 #include "board.hpp"
 
+/// perform actions defined by command line
+template<typename ErrorHandler>
+void perform(boost::asio::io_service & ios, const boost::program_options::variables_map & vm,
+   ErrorHandler handler)
+{
+//   for (const auto & p : vm)
+//   {
+//      std::cout << p.first.c_str();
+//      std::cout << "=";
+//      std::cout << boost::any_cast<std::string>(p.second.value());
+//      std::cout << std::endl;
+//   }
+
+   std::cout << vm["name"].as<std::string>() << std::endl;
+   return;
+   if (vm.count("x") && vm.count("y") && vm.count("board"))
+   {
+      // solve board
+      //ios.post();
+      //throw board_not_parsed();
+   }
+   else
+   {
+      // download board
+
+//      const std::string url = std::string("http://www.hacker.org/coil/index.php")
+//         + "?name=" + vm["name"].value() + "&password=" + vm["pass"].value();
+
+      // coil::board board(ios, url);
+   }
+}
+
+/// parse command line
 template<typename ErrorHandler>
 void parse(boost::asio::io_service & ios, int argc, char ** argv, ErrorHandler handler)
 {
    namespace po = boost::program_options;
+
+   // prefix for env vars
+   const std::string prefix = "hacker_org_";
+   const std::string base_url = "http://www.hacker.org/coil/index.php";
 
    // define options
    std::string config_file_name;
@@ -20,33 +57,34 @@ void parse(boost::asio::io_service & ios, int argc, char ** argv, ErrorHandler h
       "read board configuration from file");
 
    po::options_description board_options("Board options");
-   size_t x = 0;
-   size_t y = 0;
-   std::string squares;
    board_options.add_options()
-   ("x", po::value<size_t>(&x), "set board width") // name forced to be able to read from file
-   ("y", po::value<size_t>(&y), "set board height")
-   ("board,b", po::value<std::string>(&squares),
-      "defines board itself as a string of 'X's and '.'s (set of squares)");
+   ("x", "set board width") // name forced to be able to read from file
+   ("y", "set board height")
+   ("board,b", "defines board itself as a string of 'X's and '.'s (set of squares)");
 
    po::options_description account_options("Account options");
-   std::string name;
-   std::string pass;
    account_options.add_options()
-   ("name,n", po::value<std::string>(&name), "registered user name at hacker.org")
-   ("password,p", po::value<std::string>(&pass), "password");
+      ("url,u",
+         po::value<std::string>()->default_value(base_url), "base URL to be used for GET and POST requests"),
+      ("name,n",
+         ("registered user name at hacker.org (could be env var prefixed with '" + prefix + "')").c_str()),
+      ("password,p",
+         std::string("password (could be env var prefixed with '" + prefix + "')").c_str());
 
    // put options together
    po::options_description cmdline_options;
    cmdline_options.add(general_options).add(board_options).add(account_options);
 
    po::variables_map vm;
+
+   // get options defined in command line
    po::store(po::parse_command_line(argc, argv, cmdline_options), vm);
 
-   if (/*vm.empty() ||*/ vm.count("help"))
+   if (/*vm.empty() ||*/vm.count("help"))
    {
       std::cout << general_options << board_options << account_options << std::endl;
 
+      // return with no error
       boost::system::error_code ec;
       handler.template get<0>()(ec);
       return;
@@ -57,50 +95,26 @@ void parse(boost::asio::io_service & ios, int argc, char ** argv, ErrorHandler h
       //TODO: implement version
       std::cout << "not implemented" << std::endl;
 
+      // return with no error
       boost::system::error_code ec;
       handler.template get<0>()(ec);
    }
 
-   // read from file
    if (vm.count("file"))
    {
+      // get options defined in config file
       po::notify(vm);
-      //    std::cout << "reading options from configuration file '" << config_file_name << "'"
-      //       << std::endl;
-
-      po::options_description config_file_options;
-      config_file_options.add(board_options);
       po::store(po::parse_config_file<std::string::value_type>
-         (config_file_name.c_str(), config_file_options), vm);
+         (config_file_name.c_str(), board_options), vm);
    }
 
+   // get options defined as env vars, these should be prefixed with prefix
+   po::store(po::parse_environment(account_options, prefix), vm);
+
+   // final notify
    po::notify(vm);
 
-   if (vm.count("x") && vm.count("y") && vm.count("board"))
-   {
-      // solve board
-      //ios.post();
-      //throw board_not_parsed();
-   }
-   else
-   {
-      // download board
-
-      // if name/pass not defined, resolve it from env
-      if (!vm.count("name"))
-      {
-         name = std::getenv("name");
-      }
-
-      if (!vm.count("pass"))
-      {
-         pass = std::getenv("pass");
-      }
-
-      const std::string url = std::string("http://www.hacker.org/coil/index.php")
-         + "?name=" + name + "&password=" + pass;
-
-      coil::board board(ios, url);
-   }
+   // next action
+   ios.post(boost::bind(perform<ErrorHandler>, boost::ref(ios), vm, handler));
 }
 
--- coil/src/coil.cpp
+++ coil/src/coil.cpp
@@ -2,31 +2,32 @@
 #include <boost/asio.hpp>
 #include "cmdline-parser.hpp"
 
-void error_handler(const boost::system::error_code & ec)
-{
-   //(ec ? std::cerr : std::cout) << ec.message() << std::endl;
-   if (ec)
-   {
-      std::cerr << ec.message() << std::endl;
-   }
-}
-
 class worker
 {
 public:
    worker(int argc, char** argv)
    {
       typedef boost::function<void(const boost::system::error_code &)> error_handler_type;
-      error_handler_type eh = boost::bind(error_handler, boost::asio::placeholders::error);
+      error_handler_type eh = boost::bind(&worker::error_handler, this,
+         boost::asio::placeholders::error);
       ios.post(
          boost::bind(parse<boost::tuple<error_handler_type> >, boost::ref(ios), argc, argv,
             boost::make_tuple(eh)));
       ios.run();
-
    }
+
 private:
    boost::asio::io_service ios;
    // here we can instantiate curl::downloader, board etc..
+
+   void error_handler(const boost::system::error_code & ec)
+   {
+      //(ec ? std::cerr : std::cout) << ec.message() << std::endl;
+      if (ec)
+      {
+         std::cerr << ec.message() << std::endl;
+      }
+   }
 };
 
 int main(int argc, char** argv)
